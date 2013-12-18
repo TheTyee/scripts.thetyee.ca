71339890f6f365743a07fb71d4d8d36541a9840a
Phillip Smith <ps@phillipadsmith.com>
2013-12-18 11:59:35 -0600

    Added first pass at most emailed list

---
+++ bin/popular_threads_by_email/output_list.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/env perl
+
+use Mojo::Loader;
+use Mojo::JSON;
+use Mojo::UserAgent;
+use Mojo::Template;
+use Mojo::Util qw/ encode slurp spurt /;
+use FindBin '$Bin';
+use Readonly;
+use utf8::all;
+use autodie;
+use Data::Dumper;
+
+# Read API key and forum name from a config file using autodie
+my $config_file = slurp "$Bin/conf/popular_threads_by_email.json";
+
+my $json = Mojo::JSON->new;
+my $conf = $json->decode( $config_file );
+
+# Read the output path and filename from STDIN
+my $output_file = shift @ARGV;
+die 'No output file specified' unless $output_file;
+
+# Set any constants
+Readonly my $API      => $conf->{'api_url'};
+Readonly my $RESOURCE => '/shares/email.json';
+Readonly my $URL      => $API . $RESOURCE;
+
+# Set arguments for the API call: api_key, forum, limit, etc.
+my $args = { 
+    limit   => '10',
+};
+
+# Make request to Disqus API, check response status
+# If the status is okay, then decode the results from JSON
+my $ua   = Mojo::UserAgent->new;
+my $res = $ua->get( $URL => form => $args )->res->body;
+my $data = $json->decode( $res );
+
+# Store the array data that we're after
+my $threads = $data->{'result'}{'urls'};
+
+# Render the data in a template, TODO if we have new data
+if ( $threads ) {
+    my $loader   = Mojo::Loader->new;
+    my $template = $loader->data( __PACKAGE__, 'list' );
+    my $mt       = Mojo::Template->new;
+    my $output_html = $mt->render( $template, $threads );
+    $output_html = encode 'UTF-8', $output_html;
+
+    # Write the template output to a filehandle
+    spurt $output_html, $output_file;
+};
+
+__DATA__
+@@ list
+% my ($data) = @_;
+<ul>
+% for my $thread ( @$data ) {
+%# my $title = $thread->{'title'};
+%# $title    =~ s/The Tyee – //gi;
+    <li><a href="<%= $thread->{'url'} %>"><%= $thread->{'title'} %></a> <span>(<%= $thread->{'count'} %> shares)</span></li>
+% }
+</ul>
