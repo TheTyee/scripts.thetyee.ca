c1a118c2d943908f2a3f8a156c769160a8c221c0
Phillip Smith <ps@phillipadsmith.com>
2019-04-10 22:16:55 -0700

    Re-factoring… WIP

--- bin/whatcounts_load_events_by_subscriber/whatcounts.py
+++ bin/whatcounts_load_events_by_subscriber/whatcounts.py
@@ -1,5 +1,6 @@
 import config
 import sys
+import csv
 import requests
 import json
 import datetime
@@ -12,16 +13,20 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import event
 from sqlalchemy.schema import CreateSchema
 from sqlalchemy import DDL
-Session = sessionmaker()
 
+# Database set-up
 Base = declarative_base()
+
+# Don't try to re-create the schema if it exists
 event.listen(Base.metadata, 'before_create', DDL("CREATE SCHEMA IF NOT EXISTS whatcounts"))
 db_url = f'postgresql+psycopg2://{config.db_user}:{config.db_pass}@localhost/{config.db_name}'
-
 engine = sqlalchemy.create_engine(db_url)
+
+Session = sessionmaker()
 Session.configure(bind=engine)
 session = Session()
 
+# Database classes
 class Lead(Base):
     __tablename__ = 'leads'
     __table_args__ = {'schema': 'whatcounts'}
@@ -50,12 +55,17 @@ class Event(Base):
     def __repr__(self):
         return "<Event(subscriberid='%s', eventtype='%s')>" % (self.subscriberid, self.trackingeventtype)
 
+# Create the relationship between tables
 Lead.events = relationship("Event", order_by=Event.trackingid, back_populates="lead")
 
+# Create the tables (and schema) if necessary
 Base.metadata.create_all(engine)
 
+##################
+# Functions
+##################
 
-# Generic upsert
+# Generic upsert for adding records to the database
 def get_or_create(session, model, **kwargs):
     instance = session.query(model).filter_by(**kwargs).first()
     if instance:
@@ -66,48 +76,65 @@ def get_or_create(session, model, **kwargs):
         session.commit()
         return instance
 
-# TODO 
-# - Load records from CSV
-# - Search for each record in WC
-# - If it's there, store the record's event
+def get_data_from_csv(filename):
+    with open(filename, 'rt') as csvfile:
+        reader = csv.DictReader(csvfile)
+        leads = []
+        for row in reader:
+            leads.append(row['email']) 
+    return leads
 
+def search_for_subscriber(identifier):
+    searchforsub = {'email': 'beamc.jpc@hotmail.com'}
+    endpointforsubs = 'https://secure.whatcounts.net/rest/subscribers'
 
-# TODO move into function
-searchforsub = {'email': 'beamc.jpc@hotmail.com'}
-endpointforsubs = 'https://secure.whatcounts.net/rest/subscribers'
+    r = requests.get(endpointforsubs, params=searchforsub, auth=(config.wc_realm, config.wc_realmpw))
 
-r = requests.get(endpointforsubs, params=searchforsub, auth=(config.wc_realm, config.wc_realmpw))
+    # Request returns a list with one object
+    subscribers = r.json()
+    # Get the object
+    subscriber = subscribers[0]
 
-# Request returns a list with one object
-subscribers = r.json()
-# Get the object
-subscriber = subscribers[0]
+    # Get the ID
+    subscriberid = subscriber["subscriberId"]
+    email = subscriber["email"]
 
-# Get the ID
-subscriberid = subscriber["subscriberId"]
-email = subscriber["email"]
+def store_subscriber_data(subscriber):
+    new_lead = get_or_create(session, Lead, email=email, subscriberid=subscriberid)
 
-new_lead = get_or_create(session, Lead, email=email, subscriberid=subscriberid)
+def get_subscriber_events(subscriberid):
+    # TODO Move into function
+    # Docs: https://support.whatcounts.com/hc/en-us/articles/210396566-Report-Subscriber-Events-by-Subscriber-ID
+    # Endpoint: https://[siteurl]/rest/subcribers/[subscriberId]/events?querystring
+    # Query options: 
+    # start=[yyyy-mm-dd]
+    # end=[yyyy-mm-dd]
+    # eventType=[eventType]
+    searchforevents = {'start': '2019-03-01'}
+    endpointforevents = f'https://secure.whatcounts.net/rest/subscribers/{subscriberid}/events'
+    r = requests.get(endpointforevents, params=searchforevents, auth=(config.wc_realm, config.wc_realmpw))
+    eventsjson = r.json()
+    events = eventsjson["events"]
 
-# TODO Move into function
-# Docs: https://support.whatcounts.com/hc/en-us/articles/210396566-Report-Subscriber-Events-by-Subscriber-ID
-# Endpoint: https://[siteurl]/rest/subcribers/[subscriberId]/events?querystring
-# Query options: 
-# start=[yyyy-mm-dd]
-# end=[yyyy-mm-dd]
-# eventType=[eventType]
-searchforevents = {'start': '2019-03-01'}
-endpointforevents = f'https://secure.whatcounts.net/rest/subscribers/{subscriberid}/events'
-r = requests.get(endpointforevents, params=searchforevents, auth=(config.wc_realm, config.wc_realmpw))
-eventsjson = r.json()
-events = eventsjson["events"]
+def store_events_data(events):
+    for event in events:
+        records = get_or_create(session, Event, 
+            trackingid = event["trackingId"],
+            subscriberid = subscriberid,
+            trackingeventdate = event["trackingEventDate"],
+            trackingcampaign = event["trackingCampaignId"],
+            trackingeventtype = event["eventType"],
+            trackingclickthroughid = event["trackingClickthroughId"]
+        )
 
-for event in events:
-    records = get_or_create(session, Event, 
-        trackingid = event["trackingId"],
-        subscriberid = subscriberid,
-        trackingeventdate = event["trackingEventDate"],
-        trackingcampaign = event["trackingCampaignId"],
-        trackingeventtype = event["eventType"],
-        trackingclickthroughid = event["trackingClickthroughId"]
-    )
+if __name__ == '__main__':
+    # TODO
+    # Set a default filename, e.g., leads.csv
+    filename = sys.argv[1]
+    leads = get_data_from_csv(filename)
+    for lead in leads:
+        # Search for a match in WC
+        # If matched, store the data, get back the id
+        # Lookup the events for the id
+        # Store the events
+        print("WIP")
