c2238b91e19d45c00d54062c9059eaa58b9ccfa8
Phillip Smith <ps@phillipadsmith.com>
2019-04-14 12:33:31 -0700

    Progress indicator

--- bin/whatcounts_load_events_by_subscriber/whatcounts.py
+++ bin/whatcounts_load_events_by_subscriber/whatcounts.py
@@ -74,6 +74,15 @@ Base.metadata.create_all(engine)
 # Functions
 ##################
 
+def progress(count, total, status=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+
 # Generic upsert for adding records to the database
 def get_or_create(session, model, **kwargs):
     instance = session.query(model).filter_by(**kwargs).first()
